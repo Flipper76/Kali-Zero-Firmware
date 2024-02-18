0a5769580c21bd522c5165fbfb51a9c1ba54e66c
Flipper76 <kalicem76@hotmail.fr>
2024-02-18 13:41:19 +0100

    Ajouts, Mise à Jour et corrections

--- applications/main/nfc/plugins/supported_cards/umarsh.c
+++ applications/main/nfc/plugins/supported_cards/umarsh.c
@@ -24,15 +24,12 @@
  */
 
 #include "nfc_supported_card_plugin.h"
+#include <flipper_application.h>
 
 #include "protocols/mf_classic/mf_classic.h"
-#include <flipper_application/flipper_application.h>
 
-#include <nfc/nfc_device.h>
-#include <bit_lib/bit_lib.h>
-#include <nfc/protocols/mf_classic/mf_classic_poller_sync.h>
-
-#include <datetime/datetime.h>
+#include <bit_lib.h>
+#include <datetime.h>
 
 #define TAG "Umarsh"
 
